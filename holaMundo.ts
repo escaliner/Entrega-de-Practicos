9378298293b866d3f85f40fe05a301df36d067d5
brunoarenzana <brunoarenzana@gmail.com>
2024-09-12 19:20:04 -0300

    primer commit

---
+++ holaMundo.ts
@@ -0,0 +1,15 @@
+import * as uk from "readline-sync";
+
+let tutor: string;
+tutor = uk.question("Ingresa tu nombre tutora! ");
+console.log(`Bienvenida ${tutor} a nuestro repositorio !`);
+
+
+
+
+/*
+git status
+git add .
+git commit -m "Titulo para el cambio"
+git push origin main
+*/
