99ad5036c251f8f766010726ce1ea99256b6fd12
brunoarenzana <brunoarenzana@gmail.com>
2024-09-12 23:01:21 -0300

    personalizacion de saludo

--- holaMundo.ts
+++ holaMundo.ts
@@ -3,6 +3,7 @@ import * as uk from "readline-sync";
 let tutor: string;
 tutor = uk.question("Ingresa tu nombre tutora! ");
 console.log(`Bienvenida ${tutor} a nuestro repositorio !`);
+console.log(`Aca vas a estar entretenida corrigiendo muchas cosas! `);
 
 
 
